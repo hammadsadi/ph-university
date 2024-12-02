377a2ab9ecb3142871738351d83996255c6c4e17
hammadsadi <hammad.sadi@yahoo.com>
2024-12-02 23:14:42 +0600

    Update Generated id Logic

--- src/app/modules/users/user.utils.ts
+++ src/app/modules/users/user.utils.ts
@@ -7,13 +7,27 @@ const findLastUserentId = async () => {
     .sort({ createdAt: -1 })
     .lean();
 
-  return lastUserId?.id ? lastUserId.id.substring(6) : undefined;
+  return lastUserId?.id ? lastUserId.id : undefined;
 };
 
 // Generated ID
 export const generatedId = async (payload: TAcademicSemester | null) => {
-  // First Time ID
-  const currentId = (await findLastUserentId()) || (0).toString();
+  // First Time Default ID
+  let currentId = (0).toString();
+  const lastStudentId = await findLastUserentId();
+  // Last Student Semester 2043010001
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const currentSemesterCode = payload?.code;
+  const currentSemesterYear = payload?.year;
+  // Check
+  if (
+    lastStudentId &&
+    lastStudentSemesterCode === currentSemesterCode &&
+    lastStudentSemesterYear === currentSemesterYear
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
   // Increament Id
   let increamentId = (Number(currentId) + 1).toString().padStart(4, '0');
   increamentId = `${payload?.year}${payload?.code}${increamentId}`;
