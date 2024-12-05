8c386cf51d556e371df5776e9690eee9c61ce244
hammadsadi <hammad.sadi@yahoo.com>
2024-12-05 22:19:14 +0600

    Transaction and Rollback Functionality Added

--- src/app/modules/users/user.service.ts
+++ src/app/modules/users/user.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import config from '../../config';
 import { TUser } from './user.interface';
 import { User } from './user.model';
@@ -5,6 +7,8 @@ import { TStudent } from '../students/student.interface';
 import { Student } from '../students/student.mode';
 import { generatedId } from './user.utils';
 import { AdmissionSemester } from '../admissionSemester/admission.semester.model';
+import mongoose from 'mongoose';
+import AppError from '../../errors/AppError';
 
 // User Save to DB
 const userSaveToDB = async (password: string, payload: TStudent) => {
@@ -13,21 +17,41 @@ const userSaveToDB = async (password: string, payload: TStudent) => {
   userData.role = 'student';
   userData.password = password || config.default_password;
 
+  // Find  Admission Semester info
   const admissionSemester = await AdmissionSemester.findById(
     payload.admissionSemester,
   );
-  // Genare User ID
-  userData.id = await generatedId(admissionSemester);
+  // Init Session
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
+    // Genare User ID
+    userData.id = await generatedId(admissionSemester);
 
-  // Create User
-  const createdUser = await User.create(userData);
+    // Create User ( Transaction 1 )
+    const createdUser = await User.create([userData], { session }); // Array Return
 
-  // Create Student
-  if (Object.keys(createdUser).length) {
-    payload.id = createdUser.id;
-    payload.user = createdUser._id;
-    const result = await Student.create(payload);
+    // Validation
+    if (!createdUser.length) {
+      throw new AppError(400, 'Failed to Create User');
+    }
+    payload.id = createdUser[0].id;
+    payload.user = createdUser[0]._id;
+    // Create Student
+    const result = await Student.create([payload], { session }); //  Return Array
+    // Validation
+    if (!result.length) {
+      throw new AppError(400, 'Failed to Create Student');
+    }
+
+    // Commot or Save Data
+    await session.commitTransaction();
+    // End Session
+    await session.endSession();
     return result;
+  } catch (err) {
+    await session.abortTransaction();
+    await session.endSession();
   }
 };
 
