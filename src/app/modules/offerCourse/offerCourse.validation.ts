4b36a439d26cd130bcda9da611fa173716d0d0d5
hammadsadi <hammad.sadi@yahoo.com>
2024-12-13 22:15:15 +0600

    Course Start and End Time Comparission Functionality Added

--- src/app/modules/offerCourse/offerCourse.validation.ts
+++ src/app/modules/offerCourse/offerCourse.validation.ts
@@ -2,18 +2,45 @@ import { z } from 'zod';
 import { Days } from './offerCourse.contant';
 
 const createOfferCourseValidationSchema = z.object({
-  body: z.object({
-    semesterRegistration: z.string(),
-    academicFecaulty: z.string(),
-    academicDepartment: z.string(),
-    course: z.string(),
-    faculty: z.string(),
-    maxCapacity: z.number(),
-    section: z.number(),
-    days: z.array(z.enum([...Days] as [string, ...string[]])),
-    startTime: z.string(),
-    endTime: z.string(),
-  }),
+  body: z
+    .object({
+      semesterRegistration: z.string(),
+      academicFecaulty: z.string(),
+      academicDepartment: z.string(),
+      course: z.string(),
+      faculty: z.string(),
+      maxCapacity: z.number(),
+      section: z.number(),
+      days: z.array(z.enum([...Days] as [string, ...string[]])),
+      startTime: z.string().refine(
+        (time) => {
+          const timeRegex = /^(?:[1-9]|1\d|2[0-3]):[0-5]\d$/;
+          return timeRegex.test(time);
+        },
+        {
+          message: 'Invalid Time Format Expected "HH:MM" in 24 Houes Format',
+        },
+      ),
+      endTime: z.string().refine(
+        (time) => {
+          const timeRegex = /^(?:[1-9]|1\d|2[0-3]):[0-5]\d$/;
+          return timeRegex.test(time);
+        },
+        {
+          message: 'Invalid Time Format Expected "HH:MM" in 24 Houes Format',
+        },
+      ),
+    })
+    .refine(
+      (body) => {
+        const start = new Date(`1970-01-01T${body.startTime}:00`);
+        const end = new Date(`1970-01-01T${body.endTime}:00`);
+        return end > start;
+      },
+      {
+        message: 'Start Time Should be Before End Time!',
+      },
+    ),
 });
 
 const updateOfferCourseValidationSchema = z.object({
