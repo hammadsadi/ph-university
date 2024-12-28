e3ee02b125f109c533313a51c263f0a1c2c76ffe
hammadsadi <hammad.sadi@yahoo.com>
2024-12-29 01:15:01 +0600

    Admission Semester Route Protected

--- src/app/modules/admissionSemester/admission.semester.routes.ts
+++ src/app/modules/admissionSemester/admission.semester.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { admissionSemesterValidationSchemas } from './admission.semester.validation';
 import { AdmissionSemesterControllers } from './academic.semester.controllers';
+import authChecking from '../../middlewares/authChecking';
+import { USER_ROLE } from '../users/user.constant';
 
 // Route init
 const route = Router();
@@ -9,6 +11,7 @@ const route = Router();
 // Create Semester
 route.post(
   '/create-admission-semester',
+  authChecking(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     admissionSemesterValidationSchemas.createAdmissionSemesterValidation,
   ),
@@ -17,17 +20,30 @@ route.post(
 // Get All Admission Semester
 route.get(
   '/all-admission-semester',
+  authChecking(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   AdmissionSemesterControllers.admissionSemesterGetAllController,
 );
 // Get Single Admission Semester
 route.get(
   '/:semesterId',
+  authChecking(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
   AdmissionSemesterControllers.admissionSemesterGetSingleController,
 );
 
 // Update Single Admission Semester
 route.patch(
   '/:semesterId',
+  authChecking(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     admissionSemesterValidationSchemas.updateAdmissionSemesterValidation,
   ),
