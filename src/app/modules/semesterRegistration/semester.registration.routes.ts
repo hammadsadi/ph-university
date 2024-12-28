282875988109d01b1d810b7bf3a06864c4a5f099
hammadsadi <hammad.sadi@yahoo.com>
2024-12-29 01:17:35 +0600

    Added Semester Registration Rout Protected

--- src/app/modules/semesterRegistration/semester.registration.routes.ts
+++ src/app/modules/semesterRegistration/semester.registration.routes.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { SemesterRegistrationValidationSchemas } from './semester.registration.validation';
 import { SemesterRegisterControllers } from './semester.registration.controllers';
+import authChecking from '../../middlewares/authChecking';
+import { USER_ROLE } from '../users/user.constant';
 
 // Route init
 const route = Router();
@@ -9,6 +11,7 @@ const route = Router();
 // Create Semester Registration
 route.post(
   '/create-semester-registration',
+  authChecking(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     SemesterRegistrationValidationSchemas.createSemesterRegistrationValidationSchema,
   ),
@@ -17,14 +20,33 @@ route.post(
 // Update Semester Registration
 route.patch(
   '/:id',
+  authChecking(USER_ROLE.admin, USER_ROLE.superAdmin),
   validateRequest(
     SemesterRegistrationValidationSchemas.updateSemesterRegistrationValidationSchema,
   ),
   SemesterRegisterControllers.updateSingleSemesterRegistration,
 );
 // Get All  Semester Register
-route.get('/', SemesterRegisterControllers.getSemesterRegistration);
+route.get(
+  '/',
+  authChecking(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  SemesterRegisterControllers.getSemesterRegistration,
+);
 // Get Single Semester Registration
-route.get('/:id', SemesterRegisterControllers.getSingleSemesterRegistration);
+route.get(
+  '/:id',
+  authChecking(
+    USER_ROLE.admin,
+    USER_ROLE.superAdmin,
+    USER_ROLE.faculty,
+    USER_ROLE.student,
+  ),
+  SemesterRegisterControllers.getSingleSemesterRegistration,
+);
 
 export const SemesterRegistrationRoutes = route;
