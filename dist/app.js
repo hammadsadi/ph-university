2a98d15d7d96734eff729dec218268d5666c47d5
hammadsadi <hammad.sadi@yahoo.com>
2025-01-12 23:36:59 +0600

    Added Credientials Policy

--- dist/app.js
+++ dist/app.js
@@ -13,7 +13,7 @@ const app = (0, express_1.default)();
 // Middleware
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ['http://localhost:5173'] }));
+app.use((0, cors_1.default)({ origin: ['http://localhost:5173'], credentials: true }));
 app.get('/', (req, res) => {
     res.send('Server Running Now');
 });
